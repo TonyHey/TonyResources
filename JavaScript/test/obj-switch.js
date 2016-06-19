e4df32859a46b8376752c03f1f29ae9c40ac278d
Tony He <562359635@qq.com>
2016-06-20 02:53:03 +0800

    update
    obj switch

---
+++ JavaScript/test/obj-switch.js
@@ -0,0 +1,11 @@
+function dcf(test){
+    return({
+            cat :function(){console.log('cat');},
+            dog :function(){console.log('dog');}
+        }
+    	[test] || function(){console.log('default');}
+    )();
+}
+
+dcf();
+dcf('dog');
